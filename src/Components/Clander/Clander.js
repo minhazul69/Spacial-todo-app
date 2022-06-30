09396ddae54cea31ad4ff88844ed89b7db60465b
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-06-30 16:15:12 +0600

    calendar added successfull added

--- src/Components/Clander/Clander.js
+++ src/Components/Clander/Clander.js
@@ -1,9 +1,32 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import calenderImg from "../../image/calendar-gif.gif";
 const Clander = () => {
+  const [selected, setSelected] = useState(new Date());
+  const css = `.my-today { 
+        font-weight: bold;
+        font-size: 180%; 
+        color: red;
+      }`;
   return (
     <div>
-      <h3>This Is Clander</h3>
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <img src={calenderImg} class="max-w-sm rounded-lg shadow-2xl" />
+          <div className="lg:px-24">
+            <style>{css}</style>
+            <DayPicker
+              mode="single"
+              selected={selected}
+              onSelect={setSelected}
+              modifiersClassNames={{
+                today: "my-today",
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
